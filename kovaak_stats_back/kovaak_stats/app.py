d1450bb55baf9e418401bffea39fa2c73dec894c
Guillaume ROCHE <guillaume1.roche@staff.bocal.org>
2019-12-13 16:19:16 +0100

    Fix the conf file path

--- kovaak_stats_back/kovaak_stats/app.py
+++ kovaak_stats_back/kovaak_stats/app.py
@@ -8,7 +8,7 @@ def create_app(name=__name__, config=False):
     application = Flask(name)
     if not config:
         config = os.environ.get('KOVAAK_STATS_BACK_CONFIG_FILE',
-                                sys.prefix + '/www/console.bocal.org/app/app.conf')
+                                sys.prefix + '/www/kovaak_stats_back/app/app.conf')
     if os.path.isfile(config):
         application.config.from_pyfile(config)
     application.register_blueprint(api_bp, url_prefix='/api')
