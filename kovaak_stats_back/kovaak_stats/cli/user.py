df93c5d0e78a72f9d83a760c04f250c8b9dc6a63
Guillaume ROCHE <guillaumerocheg@gmail.com>
2020-05-02 09:00:36 +0200

    cli: add send_code, get_stats and upload_stats

--- kovaak_stats_back/kovaak_stats/cli/user.py
+++ kovaak_stats_back/kovaak_stats/cli/user.py
@@ -1,7 +1,12 @@
 import sys
 from kovaak_stats.app import db
 from kovaak_stats.models.user import User
+from kovaak_stats.utils import send_email
+from kovaak_stats.utils.users import format_users_stats
+from werkzeug.datastructures import FileStorage
 from .utils import check_user
+from datetime import datetime
+import pprint
 
 
 def subparser_install(subparser):
@@ -65,6 +70,35 @@ def subparser_install(subparser):
     parser_right_del.set_defaults(func=users_right_del)
     parser_right_del.add_argument('user', type=check_user, help='The username')
     parser_right_del.add_argument('rights', nargs='+', help='The rights to delete')
+
+    parser_send_code = subparser.add_parser(
+        'send_code',
+        help='Send a code to change the password',
+    )
+    parser_send_code.set_defaults(func=users_send_code)
+    parser_send_code.add_argument('user', type=check_user, help='The username')
+
+    parser_get_stats = subparser.add_parser(
+        'get_stats',
+        help='Show a user\'s stats',
+    )
+    parser_get_stats.set_defaults(func=users_get_stats)
+    parser_get_stats.add_argument('user', type=check_user, help='The username')
+    parser_get_stats.add_argument('--scenarii', help='Filter by scenario', nargs='*')
+    parser_get_stats.add_argument('--start', type=int,
+                                  help='Won\'t return any stats for sessions done before this timestamp',
+                                  default=None)
+    parser_get_stats.add_argument('--end', type=int,
+                                  help='Won\'t return any stats for sessions done after this timestamp',
+                                  default=None)
+
+    parser_upload_stats = subparser.add_parser(
+        'upload_stats',
+        help='Upload stats files',
+    )
+    parser_upload_stats.set_defaults(func=users_upload_stats)
+    parser_upload_stats.add_argument('user', type=check_user, help='The username')
+    parser_upload_stats.add_argument('files', help='The files', nargs='*')
 
 
 def create_user(username, email_addr, password, **kwargs):
@@ -131,4 +165,50 @@ def users_right_del(user, rights, **kwargs):
         except ValueError as e:
             print(e, file=sys.stderr)
             sys.exit(1)
+    db.session.commit()
+
+
+def users_send_code(user, **kwargs):
+    code = user.gen_recovery_code()
+    if not code:
+        print('A code has already been sent in the last 10 minutes', file=sys.stderr)
+        sys.exit(1)
+    if send_email('noreply@kovaakstatsviewer.com', user.email_addr,
+                  'Recovery code kovaak stats viewer',
+                  'The code to change your kovaak stats viewer password is {}'.format(code)) is False:
+        user.recovery_code.delete()
+        db.session.commit()
+        print('Couldn\'t send an email with the code', file=sys.stderr)
+        sys.exit(1)
+    print('A code has been sent to the email address associated to {}\'s account'.format(user.name))
+
+
+def users_get_stats(user, scenarii, start, end, **kwargs):
+    start = datetime.fromtimestamp(start) if start else datetime.fromtimestamp(0)
+    end = datetime.fromtimestamp(end) if end else datetime.fromtimestamp(2147483647)
+    from kovaak_stats.models.stat import Stat
+    if scenarii:
+        stats = Stat.query.filter_by(user_id=user.id).filter(Stat.scenario.in_(scenarii),
+                                                             Stat.creation_date >= start,
+                                                             Stat.creation_date <= end).order_by(
+            Stat.execution_date).all()
+    else:
+        stats = Stat.query.filter_by(user_id=user.id).filter(Stat.creation_date >= start,
+                                                             Stat.creation_date <= end).order_by(
+            Stat.execution_date).all()
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(format_users_stats(stats))
+
+
+def users_upload_stats(user, files, **kwargs):
+    from kovaak_stats.models.stat import Stat
+    for filename in files:
+        print('Uploading {}'.format(filename))
+        file = FileStorage(stream=open(filename, "rb"), filename=filename, content_type='text/csv')
+        try:
+            Stat.create(file, user)
+        except ValueError as e:
+            print('Warning: {}'.format(e), file=sys.stderr)
+        except IndexError as e:
+            print('Warning: wrong file format'.format(filename), file=sys.stderr)
     db.session.commit()
