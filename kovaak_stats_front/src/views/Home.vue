eeddb3cbf54fb1950c7d7f5dd7e16cb700643a49
Guillaume ROCHE <guillaumerocheg@gmail.com>
2020-05-07 08:41:10 +0200

    gui: upload the updated .exe; front: add a download button

--- kovaak_stats_front/src/views/Home.vue
+++ kovaak_stats_front/src/views/Home.vue
@@ -19,9 +19,18 @@
       <h5 class="mb-4">
         It only takes a few clicks to get ready
       </h5>
-      <div class="text-center">
+      <div class="text-center mb-4">
         <router-link to="/login"><b-button variant="outline-dark" class="mr-2 mb-2" size="lg">Sign-in</b-button></router-link>
         <router-link to="/signup"><b-button variant="primary" class="mr-2 mb-2" size="lg">Sign-up</b-button></router-link>
+      </div>
+
+      <h5 class="mb-4">
+        You can also use the Windows 10 compatible GUI to upload your stats more easily after having created your account
+      </h5>
+      <div class="text-center mb-4">
+        <b-link href="https://github.com/ftsn/kovaak-stats-viewer/blob/master/gui/main.exe">
+          <b-button variant="outline-primary" class="mb-2" size="lg">Download it here</b-button>
+        </b-link>
       </div>
     </b-jumbotron>
     </b-col>
